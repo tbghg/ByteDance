b1ea0269d7303acf7b32929e623da2a499a61c2a
田冰航 <1312186639@qq.com>
2022-05-27 10:25:13 +0800

    1.修改了注册返回的结构体格式（server与controller结构体命名重复了） 2.稍微写了下登录，但是还没写完，考虑到接下来要考试先提交一下

--- cmd/user/controller/query_user_info.go
+++ cmd/user/controller/query_user_info.go
@@ -8,19 +8,28 @@ import (
 	"net/http"
 )
 
-type RegData struct {
+// 用户登录返回值
+type loginResponse struct {
 	common.Response
 	UserID int    `json:"user_id"`
 	Token  string `json:"token"`
 }
 
+// 用户注册返回值
+type regUserResponse struct {
+	common.Response
+	UserID int    `json:"user_id"`
+	Token  string `json:"token"`
+}
+
+// RegisterUser 注册用户
 func RegisterUser(c *gin.Context) {
 	username := c.Query("username")
 	password := c.Query("password")
 	regUserData, isExist := service.RegUser(username, password)
 
 	if isExist {
-		c.JSON(http.StatusOK, RegData{
+		c.JSON(http.StatusOK, regUserResponse{
 			Response: common.Response{
 				StatusCode: -1,
 				StatusMsg:  msg.AlreadyRegisteredStatusMsg,
@@ -28,7 +37,7 @@ func RegisterUser(c *gin.Context) {
 		})
 	}
 
-	c.JSON(http.StatusOK, RegData{
+	c.JSON(http.StatusOK, regUserResponse{
 		Response: common.Response{
 			StatusCode: 0,
 			StatusMsg:  msg.RegisterSuccessStatusMsg,
@@ -36,4 +45,29 @@ func RegisterUser(c *gin.Context) {
 		UserID: regUserData.ID,
 		Token:  regUserData.Token,
 	})
+}
+
+func LoginUser(c *gin.Context) {
+	username := c.Query("username")
+	password := c.Query("password")
+	loginData, isCorrect := service.LoginUser(username, password)
+	if !isCorrect {
+		// 账号或密码错误
+		c.JSON(http.StatusOK, loginResponse{
+			Response: common.Response{
+				StatusCode: -1,
+				StatusMsg:  msg.WrongUsernameOrPasswordMsg,
+			},
+		})
+	} else {
+		// 登陆成功
+		c.JSON(http.StatusOK, loginResponse{
+			Response: common.Response{
+				StatusCode: 0,
+				StatusMsg:  msg.LoginSuccessStatusMsg,
+			},
+			UserID: loginData.ID,
+			Token:  loginData.Token,
+		})
+	}
 }
