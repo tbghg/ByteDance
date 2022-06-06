d39f28c748f954523b59084b2c3ecd8b9b6003ac
db <86559454+Barsit@users.noreply.github.com>
2022-06-06 20:35:10 +0800

    增加评论（撤销）操作、评论列表接口

--- cmd/comment/service/query_comment_info.go
+++ cmd/comment/service/query_comment_info.go
@@ -1,28 +1,55 @@
 package service
 
 import (
+	"ByteDance/cmd/comment"
 	"ByteDance/cmd/comment/repository"
+	"ByteDance/cmd/video"
+	"ByteDance/dal/method"
 	"ByteDance/utils"
+	"fmt"
+
+	"github.com/araddon/dateparse"
 )
 
-type CommentDaoRequest struct {
-	UserId      int64
-	Token       string
-	VideoId     int64
-	ActionType  int32
-	CommentText string
-	CommentId   int64
-}
-
-func RelationAction(userId int32, videoId int32, commentText string, actionType int32) (err error) {
+func RelationAction(userId int32, videoId int32, commentText string, commentId int32) (err error) {
 	//更新 如果数据库没有该数据则返回IsExist = 0
-	IsExist := repository.CommentDao.RelationUpdate(userId, videoId, actionType)
+	IsExist := repository.CommentDao.RelationUpdate(commentId)
 
-	if IsExist == 0 {
+	//commentText不空，创建评论
+	if IsExist == 0 && len(commentText) != 0 {
 		//添加该数据
 		err = repository.CommentDao.RelationCreate(userId, videoId, commentText)
 		utils.CatchErr("添加失败", err)
 	}
 
 	return err
+}
+
+//评论列表
+func RelationList(videoId int32) (commentInfo []comment.TheCommentInfo, state int) {
+	allCommentInfoData, _ := repository.CommentDao.RelationSelect(videoId)
+
+	commentInfo = make([]comment.TheCommentInfo, len(allCommentInfoData))
+
+	for index, commentInfoData := range allCommentInfoData {
+		//获取关注相关
+		followerCount, followCount, _ := method.QueryFollowCount(commentInfoData.UserID)
+		//评论时间转换
+		createDate, _ := dateparse.ParseAny(commentInfoData.CreateDate)
+		fmt.Println(createDate)
+		commentInfo[index] = comment.TheCommentInfo{
+			ID: commentInfoData.ID,
+			User: video.AuthorInfo{
+				ID:            commentInfoData.UserID,
+				Name:          commentInfoData.Username,
+				FollowCount:   int(followCount),
+				FollowerCount: int(followerCount),
+				IsFollow:      false,
+			},
+			Content:    commentInfoData.Content,
+			CreateDate: createDate.Format("01-02"),
+		}
+	}
+
+	return commentInfo, 1
 }
