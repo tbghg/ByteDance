8a1b0bd87cf3c5bb4585b618032772de69451353
db <86559454+Barsit@users.noreply.github.com>
2022-06-08 17:14:54 +0800

    初始化redis

--- server.go
+++ server.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"ByteDance/pkg/middleware"
 	"ByteDance/utils"
+	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// 启动redis
+	err1 := middleware.InitClient()
+	if err1 != nil {
+		//redis连接错误
+		panic(err1)
+	}
+	fmt.Println("Redis连接成功")
+
 	r := gin.Default()
 	initRouter(r) // 初始化路由
 	err := r.Run(":8000")
