bc863b9e1dd3caae5488e65f3830bb4acdd60f60
田冰航 <1312186639@qq.com>
2022-06-08 11:39:59 +0800

    添加了查询方法

--- dal/method/method.go
+++ dal/method/method.go
@@ -1,7 +1,28 @@
 package method
 
 // UserMethod Method 指定自定义查询方法
-type UserMethod interface {
+type FollowMethod interface {
+	// 查询用户粉丝数
+	//
+	//select count(1) from follow Where user_id = @userID and removed = 0 and deleted = 0
+	QueryFollowerCount(userID int32) int64
+
+	// 查询粉丝数目
+	//
+	//select count(1) from follow Where fun_id = @funID and removed = 0 and deleted = 0
+	QueryFollowCount(funID int32) int64
 }
 
-// 其他同理
+type FavoriteMethod interface {
+	// 查询视频点赞数目
+	//
+	// select count(1) from favorite where video_id = @videoID and removed = 0 and deleted = 0
+	QueryFavoriteCount(videoID int32) int64
+}
+
+type CommentMethod interface {
+	// 查询视频评论数目
+	//
+	// select count(1) from comment where video_id = @videoID and removed = 0 and deleted = 0
+	QueryCommentCount(videoID int32) int64
+}
