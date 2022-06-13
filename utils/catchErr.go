182e5c55bf631e3d7def1b8fe0716b8e18da40fd
田冰航 <1312186639@qq.com>
2022-06-13 11:05:45 +0800

    添加判断，提高效率

--- utils/catchErr.go
+++ utils/catchErr.go
@@ -7,10 +7,10 @@ import (
 
 // CatchErr 捕捉错误
 func CatchErr(logInfo string, err error) bool {
-	logFile, _ := os.OpenFile("../logs/ByteDance.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	log.SetOutput(logFile)
-	log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
 	if err != nil {
+		logFile, _ := os.OpenFile("../logs/ByteDance.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		log.SetOutput(logFile)
+		log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
 		log.Printf(logInfo+": %s\n", err)
 		return false
 	}
