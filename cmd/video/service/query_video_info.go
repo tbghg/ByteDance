6abf75ba4524a6bb888a7384bd80efdd605232aa
田冰航 <1312186639@qq.com>
2022-06-15 10:42:49 +0800

    上传视频，获取封面并上传二者并发进行

--- cmd/video/service/query_video_info.go
+++ cmd/video/service/query_video_info.go
@@ -56,35 +56,48 @@ func GetVideoFeed(lastTime int64, userID int32) (nextTime int64, videoInfo []vid
 	return nextTime, videoInfo, 1
 }
 
-func PublishVideo(userID int, title string, fileBytes []byte) bool {
+func PublishVideo(userID int, title string, fileBytes []byte) (success bool) {
 	node, _ := utils.NewWorker(1)
 	randomId := node.GetId()
 	fileID := fmt.Sprintf("%v", randomId)
+	success = true
 
-	if !utils.UploadFile(fileBytes, fileID, "video") {
-		return false
-	}
-	// 通过ffmpeg截取视频第一帧为视频封面
-	videoURL := common.OSSPreURL + fileID + ".mp4"
-	cmd := exec.Command("ffmpeg", "-i", videoURL, "-vframes", "1", "-f", "singlejpeg", "-")
-	buf := new(bytes.Buffer)
-	cmd.Stdout = buf
-	err := cmd.Run()
-	if !utils.CatchErr("ffmpeg运行错误", err) {
-		return false
-	}
-	// 将视频封面上传至OSS中
-	if !utils.UploadFile(buf.Bytes(), fileID, "picture") {
-		return false
-	}
-	// 存入数据库中
-	success := repository.VideoDao.PublishVideo(userID, title, fileID)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func(success *bool) {
+		if !utils.UploadFile(fileBytes, fileID, "video") {
+			*success = false
+		}
+		wg.Done()
+	}(&success)
+
+	go func(success *bool) {
+		// 通过ffmpeg截取视频第一帧为视频封面
+		videoURL := common.OSSPreURL + fileID + ".mp4"
+		cmd := exec.Command("ffmpeg", "-i", videoURL, "-vframes", "1", "-f", "singlejpeg", "-")
+		buf := new(bytes.Buffer)
+		cmd.Stdout = buf
+		err := cmd.Run()
+		if !utils.CatchErr("ffmpeg运行错误", err) {
+			*success = false
+		}
+		// 将视频封面上传至OSS中
+		if !utils.UploadFile(buf.Bytes(), fileID, "picture") {
+			*success = false
+		}
+		wg.Done()
+	}(&success)
+	wg.Wait()
 	if success {
-		return true
+		if repository.VideoDao.PublishVideo(userID, title, fileID) {
+			return true
+		} else {
+			return false
+		}
 	} else {
 		return false
 	}
-
 }
 
 func PublishList(userID int32) (videoInfo []video.TheVideoInfo, success bool) {
