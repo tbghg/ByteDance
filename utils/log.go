621b7501f440fadf3fb43a8509cc25856906ea74
田冰航 <1312186639@qq.com>
2022-06-15 12:59:52 +0800

    完善日志功能

---
+++ utils/log.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+var Log *zap.Logger
+
+func LogConfig() {
+	//获取编码器,NewJSONEncoder()输出json格式，NewConsoleEncoder()输出普通文本格式
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder //指定时间格式
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoder := zapcore.NewConsoleEncoder(encoderConfig)
+
+	//文件writeSyncer
+	fileWriteSyncer := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   "./logs/ByteDance.log", //日志文件存放目录
+		MaxSize:    1,                      //文件大小限制,单位MB
+		MaxBackups: 5,                      //最大保留日志文件数量
+		MaxAge:     30,                     //日志文件保留天数
+		Compress:   false,                  //是否压缩处理
+	})
+	fileCore := zapcore.NewCore(encoder, zapcore.NewMultiWriteSyncer(fileWriteSyncer, zapcore.AddSync(os.Stdout)), zapcore.InfoLevel) //第三个及之后的参数为写入文件的日志级别,ErrorLevel模式只记录error级别的日志
+
+	Log = zap.New(fileCore, zap.AddCaller()) //AddCaller()为显示文件名和行号
+
+}
